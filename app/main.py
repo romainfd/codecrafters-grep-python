2b5f25efeff7a3f4d5fd3b1f0ae22657c61e5330
Romain Fouilland <romain.fd@gmail.com>
2024-09-15 15:48:41 +0200

    Stage 6: Combining character classes

--- app/main.py
+++ app/main.py
@@ -15,9 +15,9 @@ def match_pattern(input_line, pattern):
     # Recurrence: tries to find 1st element of pattern
     # We build input_line_next and pattern_next that will be used for the next match_pattern call
     current_char, input_line_next, pattern_next = input_line[0], input_line[1:], pattern
-    if pattern == r"\d" and match_digit(input_line[0]):
+    if pattern.startwith(r"\d") and match_digit(input_line[0]):
         pattern_next = pattern[2:]
-    elif pattern == r"\w" and match_alphanumeric(input_line[0]):
+    elif pattern.startswith(r"\w") and match_alphanumeric(input_line[0]):
         pattern_next = pattern[2:]
     elif pattern.startswith('['):
         closingBracketIndex = pattern.index(']')
@@ -28,10 +28,12 @@ def match_pattern(input_line, pattern):
         # We perform direct match
         if input_line[0] == pattern[0]:
             pattern_next = pattern[1:]
+    print(input_line_next, pattern_next)
     return match_pattern(input_line_next, pattern_next)
 
 
 def match_digit(input_line):
+    print(input_line)
     # return any([str(d) in input_line for d in range(10)])
     return match_positive_character_group(input_line, '0123456789')
 
